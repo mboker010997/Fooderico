f25455219f93fb6a2ff2e266d9acda7e34cff214
Бакыт Турдиев <uncontrollablebakyt@gmail.com>
2024-03-11 01:58:42 +0300

    add photos states

---
+++ src/statemachine/state/photos/PhotoListState.py
@@ -0,0 +1,22 @@
+from src.statemachine.State import State
+from src.statemachine.state.photos.PhotosState import PhotosState
+from aiogram import types
+
+
+class PhotoListState(State):
+    def __init__(self, photo_file_ids):
+        self.photo_file_ids = photo_file_ids
+
+    def processUpdate(self, message: types.Message):
+        pass
+
+    def getNextState(self, message: types.Message):
+        return PhotosState(self.photo_file_ids)
+
+    async def sendMessage(self, message: types.Message, bot, dp):
+        if not self.photo_file_ids:
+            await message.answer("Вы еще не загрузили ни одной фотографии")
+            return
+        await message.answer("Ваши фотографии:")
+        for photo_file_id in self.photo_file_ids:
+            await message.answer_photo(photo=photo_file_id)
