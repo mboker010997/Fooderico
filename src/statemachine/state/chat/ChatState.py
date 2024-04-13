89dc1dd9e26bb52208bca07edddf62aa68926fa7
Shirokov Kirill <kir100803@gmail.com>
2024-04-13 10:28:36 +0300

    FOOD-78 add go to menu

--- src/statemachine/state/chat/ChatState.py
+++ src/statemachine/state/chat/ChatState.py
@@ -1,6 +1,7 @@
 from src.statemachine import State
 from src.model import Update
 from src import bot
+from aiogram import types
 from src.statemachine.state import menu
 
 
@@ -10,11 +11,23 @@ class ChatState(State):
         self.other_chat_id = self.context.other_chat_id
 
     def processUpdate(self, update: Update):
-        self.context.setState(menu.MenuState(self.context))
+        if not update.getMessage():
+            return
+        text = update.getMessage().text
+        if text == self.context.getMessage("menuBtn"):
+            self.context.setState(menu.MenuState(self.context))
+        # else:
+        #     bot.send_message(self.other_chat_id, text)
         self.context.saveToDb()
 
     async def sendMessage(self, update: Update):
+        # if not update.getMessage():
+        #     return
         callback = update.getCallbackQuery()
         other_user = bot.DBController().getUserByChatId(self.other_chat_id)
-        await callback.message.answer(f"Вы попали в анонимный чат с {other_user.profile_name}")
+        kb = [
+            [types.KeyboardButton(text=self.context.getMessage("menuBtn"))],
+        ]
+        keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True, one_time_keyboard=True)
+        await callback.message.answer(f"Вы попали в анонимный чат с {other_user.profile_name}", reply_markup=keyboard)
         await callback.answer()
