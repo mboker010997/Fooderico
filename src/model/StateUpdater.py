570e19e50d156ee0e1c75094e8837ef1b1a9a6b9
Shirokov Kirill <kir100803@gmail.com>
2024-04-20 11:28:14 +0300

    FOOD-89 snake_case in StateUpdater

--- src/model/StateUpdater.py
+++ src/model/StateUpdater.py
@@ -2,37 +2,37 @@ from src import bot
 from src.statemachine.state import *
 from src import statemachine
 
-stateCacheHolder = dict()
+state_cache_holder = dict()
 
 
 class StateUpdater:
     @staticmethod
     def get_state(chat_id):
-        context = stateCacheHolder.get(chat_id, None)
+        context = state_cache_holder.get(chat_id, None)
         if context is None:
             user = bot.DBController().getUserByChatId(chat_id)
-            stateClass = user.state_class
-            if stateClass is None:
+            state_class = user.state_class
+            if state_class is None:
                 return InitialState()
             context = statemachine.Context(None, user)
-            context.state = stateClass(context)
-            stateCacheHolder[chat_id] = context
+            context.state = state_class(context)
+            state_cache_holder[chat_id] = context
         return context.state
 
     @staticmethod
     def get_context(chat_id) -> statemachine.Context:
-        context = stateCacheHolder.get(chat_id, None)
+        context = state_cache_holder.get(chat_id, None)
         return context
 
     @staticmethod
     def get_sent_message(chat_id):
-        context = stateCacheHolder.get(chat_id, None)
+        context = state_cache_holder.get(chat_id, None)
         if context is None:
             return None
         return context.get_sent_message()
 
     @staticmethod
     def set_sent_message(chat_id, message):
-        context = stateCacheHolder.get(chat_id, None)
+        context = state_cache_holder.get(chat_id, None)
         if context is not None:
             context.set_sent_message(message)
