913db56f149af981613cfd3e8f1beaea1a334548
Dmitry Osipov <osipovdmitry19012015@gmail.com>
2024-05-11 01:16:03 +0300

    FOOD-11: add main admins

--- src/bot/config.py
+++ src/bot/config.py
@@ -4,4 +4,4 @@ password = "root"
 db_name = "telebot"
 port = 5432
 nominatim_URL = "http://nominatim.openstreetmap.org"
-main_admin = 1007570423
+main_admin = [1007570423, 801924891, 568426183, 533199662, 243177615, 504448004]
